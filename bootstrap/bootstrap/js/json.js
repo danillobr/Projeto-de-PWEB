d9e203a908660333c879b0af81b5523982a5310b
Danillo Rodrigues Abreu <danilo100kl@hotmail.com>
2016-05-22 19:59:28 -0300

    Uso de json para cadastrar os links dos videos! Pagina de assisitr anines do Naruto Shippuuden Pronta!

---
+++ bootstrap/bootstrap/js/json.js
@@ -0,0 +1,41 @@
+function json(){
+	return [{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2001%2002.MP4",
+			titulo:"Episódio 01, 02 - Volta a Casa, A Akatsuki Se Move",
+			link:0},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2003.MP4",
+			titulo:"Episódio 03 - Os Resultados do Treino",
+			link:1},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2004.MP4",
+			titulo:"Episódio 04 - O Jinchuuriki da Areia",
+			link:2},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2005.MP4",
+			titulo:"Episódio 05 - Como Kazekage…!",
+			link:3},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2006%2007.MP4",
+			titulo:"Episódio 06, 07 - A Arte é uma Explosão!; Corra, Kankuro, Corra!",
+			link:4},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2008%2009.MP4",
+			titulo:"Episódio 08, 09 - Time Kakashi se prepara!; As Lágrimas de um Jinchuriki!",
+			link:5},
+
+			{linkVideo:"http://1temp908.cam.fairytaill.com/Naruto%20Shippuden/Naruto%20Shippuden%2010.MP4",
+			titulo:"Episódio 10 - Técnica de Selamento – Os Nove Selos do Dragão Fantasma!",
+			link:6}
+			];
+}
+
+function _get(){
+ var link=window.location.href;
+ f=link.split("?")
+ return f[1];
+}
+
+
+
+
+
